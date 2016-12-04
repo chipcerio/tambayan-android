87eb5b8a76a031de7fc82a3f8ed15586152e5388
Alistair Cerio <alistair.cerio@gmail.com>
2016-12-04 22:31:01 +0800

    upload image to firebase storage

--- app/src/main/java/com/chipcerio/tambayan/add/AddActivity.java
+++ app/src/main/java/com/chipcerio/tambayan/add/AddActivity.java
@@ -1,6 +1,12 @@
 package com.chipcerio.tambayan.add;
 
+import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +21,8 @@ import android.widget.Toast;
 import com.chipcerio.tambayan.R;
 import com.chipcerio.tambayan.model.pojo.Event;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuth.AuthStateListener;
@@ -25,15 +33,32 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class AddActivity extends AppCompatActivity implements AuthStateListener {
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+import pub.devrel.easypermissions.EasyPermissions.PermissionCallbacks;
+
+public class AddActivity extends AppCompatActivity implements AuthStateListener, PermissionCallbacks {
 
     private static final String TAG = "AddActivity";
+
+    private static final int RC_DISK_PERM = 100;
+
+    private static final int RC_SETTINGS_SCREEN = 101;
+
+    private static final int PICK_IMAGE = 200;
 
     private FirebaseAuth mAuth;
 
@@ -44,17 +69,20 @@ public class AddActivity extends AppCompatActivity implements AuthStateListener 
     private FirebaseStorage mStorage;
 
     private StorageReference mStorageRef;
-
     private EditText mTitle;
     private EditText mDesc;
     private EditText mLoc;
     private EditText mPrice;
     private EditText mDateTime;
-    private Button mSelectImage;
 
+    private Button mSelectImage;
     private Button mSelectCategory;
     private Map<String, String> categories;
+
     private List<String> listCategories = new ArrayList<>();
+    private AlertDialog dialog;
+    private String selectedCategory;
+    private StorageReference mImageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,6 +109,7 @@ public class AddActivity extends AppCompatActivity implements AuthStateListener 
         mSelectImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                readDiskTask();
             }
         });
 
@@ -93,8 +122,131 @@ public class AddActivity extends AppCompatActivity implements AuthStateListener 
         });
 
         mStorage = FirebaseStorage.getInstance();
-        mStorageRef = mStorage.getReferenceFromUrl("gs://tambayan-ios-rey.appspot.com/eventImage/");
-        // gs://tambayan-ios-rey.appspot.com/eventImage/
+        mStorageRef = mStorage.getReferenceFromUrl("gs://tambayan-ios-rey.appspot.com/");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case RC_SETTINGS_SCREEN:
+                Toast.makeText(this, R.string.returned_from_app_settings_to_activity, Toast.LENGTH_SHORT).show();
+                break;
+
+            case PICK_IMAGE:
+                if (resultCode == RESULT_OK) {
+                    Uri imageUri = data.getData();
+                    String path = getAbsolutePathFromUri(imageUri);
+                    uploadImage(path);
+                    mSelectImage.setText(path);
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void uploadImage(String path) {
+        // File or Blob
+        File file = new File(path);
+        Uri uri = Uri.fromFile(file);
+
+        // Create the file metadata
+        StorageMetadata metadata = new StorageMetadata.Builder()
+                .setContentType("image/jpeg")
+                .build();
+
+        try {
+            // Upload file and metadata to the path 'images/filename.jpg'
+            InputStream stream = new FileInputStream(file);
+            UploadTask uploadTask = mStorageRef.child("images/" + uri.getLastPathSegment()).putStream(stream, metadata);
+
+            // Listen for state changes, errors, and completion of the upload.
+            uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                    System.out.println("Upload is " + progress + "% done");
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUrl = taskSnapshot.getMetadata().getDownloadUrl();
+                    Log.d(TAG, "UploadTask.onSuccess:" + downloadUrl.getPath());
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e(TAG, "UploadTask.onFailure:" + e.getMessage());
+                }
+            });
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getAbsolutePathFromUri(Uri imageUri) {
+        String wholeId = DocumentsContract.getDocumentId(imageUri);
+        // Split at colon, use second item in the array
+        String id = wholeId.split(":")[1];
+
+        String sel = MediaStore.Images.Media._ID + "=?";
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = getContentResolver().query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                projection,
+                sel,
+                new String[]{ id },
+                null
+        );
+
+        int columnIndex = cursor.getColumnIndex(projection[0]);
+        String picturePath = "";
+        if (cursor.moveToFirst()) {
+            picturePath = cursor.getString(columnIndex); // returns null
+        }
+        cursor.close();
+
+        Log.d(TAG, "imagePath:" + picturePath);
+
+        return picturePath;
+    }
+
+    @AfterPermissionGranted(RC_DISK_PERM)
+    private void readDiskTask() {
+        if (EasyPermissions.hasPermissions(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            Toast.makeText(this, "Has Permission", Toast.LENGTH_LONG).show();
+
+            Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            getIntent.setType("image/*");
+            Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+
+            startActivityForResult(chooserIntent, PICK_IMAGE);
+
+        } else {
+            // Ask for one permission
+            EasyPermissions.requestPermissions(this, getString(R.string.rationale_read_storage),
+                    RC_DISK_PERM, Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+        Log.d(TAG, "onPermissionsGranted:" + requestCode + ":" + perms.size());
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+        Log.d(TAG, "onPermissionsDenied:" + requestCode + ":" + perms.size());
     }
 
     private void getCategories() {
@@ -115,9 +267,6 @@ public class AddActivity extends AppCompatActivity implements AuthStateListener 
             }
         });
     }
-
-    private AlertDialog dialog;
-    private String selectedCategory;
 
     private void showDialog(List<String> list) {
         if (list.isEmpty()) {
@@ -249,5 +398,4 @@ public class AddActivity extends AppCompatActivity implements AuthStateListener 
             Toast.makeText(this, mFirebaseUser.getEmail(), Toast.LENGTH_SHORT).show();
         }
     }
-
 }
